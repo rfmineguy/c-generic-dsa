0e4429b4a44ce61ff9aa2cf2396d8d5fa9a979e3
Riley Fischer <rfmineguy@gmail.com>
2025-11-26 08:41:59 -0800

    dsaimpls: implement queue_avl_node specialty

---
+++ dsaimpls/queue_avl_node.c
@@ -0,0 +1,6 @@
+#include "../dsalib/avl.h"
+#define type_type avl_node
+#define type_name avl_node
+#define ALLOW_DIRECT_INCLUDE
+
+#include "../dsalib/queue.c"
---
+++ dsaimpls/queue_avl_node.h
@@ -0,0 +1,9 @@
+#ifndef QUEUE_AVL_NODE_H
+#define QUEUE_AVL_NODE_H
+#define type_type avl_node*
+#define type_name avl_node
+
+struct avl_node;
+#include "queue.h"
+
+#endif
