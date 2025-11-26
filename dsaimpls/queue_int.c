23ea3045188ca6f8a86f1e1323c4b34650962c93
Riley Fischer <rfmineguy@gmail.com>
2025-11-26 08:41:14 -0800

    dsaimpls: implment queue_int specialty

---
+++ dsaimpls/queue_int.c
@@ -0,0 +1,5 @@
+#define type_type int
+#define type_name int
+#define ALLOW_DIRECT_INCLUDE
+
+#include "../dsalib/queue.c"
---
+++ dsaimpls/queue_int.h
@@ -0,0 +1,8 @@
+#ifndef QUEUE_INT_H
+#define QUEUE_INT_H
+
+#define type_type int
+#define type_name int
+#include "../dsalib/queue.h"
+
+#endif
