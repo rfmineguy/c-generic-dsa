19731c1bc87887947c331f8f5d71a81985451a5a
Riley Fischer <rfmineguy@gmail.com>
2025-11-28 11:35:08 -0800

    dsalib(queue): use dsa header, and namespace q macros

--- dsalib/queue.c
+++ dsalib/queue.c
@@ -1,15 +1,15 @@
 #include "queue.h"
 #include <stdlib.h>
 #include <stdio.h>
-#ifndef ALLOW_DIRECT_INCLUDE
+#ifndef Q_ALLOW_DIRECT_INCLUDE
 #error "This file is not intended to be compiled directly"
 #endif
 
-q() func(q, new)() {
+q() qfunc(q, new)() {
   q() q = {0};
   return q;
 }
-void func(q, free)(q()* q) {
+void qfunc(q, free)(q()* q) {
   if (!q->head) return;
   q_node()* n = q->head;
   while (n) {
@@ -20,7 +20,7 @@ void func(q, free)(q()* q) {
   q->head = 0;
   q->tail = 0;
 }
-q_node()* func(q, enqueue)(q() *q, type_type val) {
+q_node()* qfunc(q, enqueue)(q() *q, q_type_type val) {
   q_node()* n = calloc(1, sizeof(q_node()));
   if (!n) return 0;
   n->val = val;
@@ -35,23 +35,23 @@ q_node()* func(q, enqueue)(q() *q, type_type val) {
   q->tail = n;
   return n;
 }
-q_node()* func(q, dequeue)(q() *q) {
+q_node()* qfunc(q, dequeue)(q() *q) {
   if (!q->head) return 0;
   q_node()* n = q->head;
   q->head = q->head->next;
   if (!q->head) q->tail = 0;
   return n;
 }
-type_type* func(q, front)(const q() *q) {
+q_type_type* qfunc(q, front)(const q() *q) {
   if (!q->head) return 0;
   return &q->head->val;
 }
-bool func(q, empty)(const q() *q) {
+bool qfunc(q, empty)(const q() *q) {
   return q->head == q->tail && !q->head;
 }
-void func(q, print)(q() *q) {
+void qfunc(q, print)(q() *q) {
   for (q_node()* n = q->head; n != q->tail; n = n->next) {
-    printf("%d", n->val);
+    qfunc(q, print_node_val)(n->val);
     if (n->next) printf(", ");
   }
   printf("\n");
--- dsalib/queue.h
+++ dsalib/queue.h
@@ -1,49 +1,43 @@
-#ifndef QUEUE_H
-#define QUEUE_H
 #include <stdbool.h>
+#include "dsa.h"
+#undef func
+#undef q
+#undef q_node
 
-#ifndef type_type
-#define type_type int
-#error "Must define type_type macro"
+#ifndef q_type_type
+#define q_type_type int
+#error "Must define q_type_type macro"
 #endif
 
-#ifndef type_name
-#define type_name int
-#error "Must define type_name macro"
+#ifndef q_type_name
+#define q_type_name int
+#error "Must define q_type_name macro"
 #endif
 
-#define JOIN2(a, b, sep) a##sep##b
-#define join2(a, b, sep) JOIN2(a,b,sep)
-
-#define JOIN3(a, b, c, sep) a##sep##b##sep##c
-#define join3(a, b, c, sep) JOIN3(a,b,c,sep)
-
-#define func_paste3(a, b, c) a##_##b##_##c
-#define func_expand(a, b, c) func_paste3(a, b, c)
-#define func(namespace, name)\
-  func_expand(namespace, type_name, name)
+#define qfunc(namespace, name)\
+  func_expand3(namespace, q_type_name, name)
 
 #define q()\
-  join2(q, type_name, _)
+  join2(q, q_type_name, _)
 
 #define q_node()\
-  join3(q, type_name, node, _)
+  join3(q, q_type_name, node, _)
 
 typedef struct q_node() {
-  type_type val;
+  q_type_type val;
   struct q_node() *next, *prev;
 } q_node();
-typedef struct {
+typedef struct q() {
   q_node() *head, *tail;
 } q();
 
-q()         func(q, new)();
-void        func(q, free)(q()* q);
-q_node()*   func(q, enqueue)(q()* q, type_type val);
-q_node()*   func(q, dequeue)(q()* q);
-type_type*  func(q, front)(const q()* q);
-bool        func(q, empty)(const q()* q);
+void        qfunc(q, print_node_val)(q_type_type val);
 
-void        func(q, print)(q()* q);
+q()         qfunc(q, new)();
+void        qfunc(q, free)(q()* q);
+q_node()*   qfunc(q, enqueue)(q()* q, q_type_type val);
+q_node()*   qfunc(q, dequeue)(q()* q);
+q_type_type*qfunc(q, front)(const q()* q);
+bool        qfunc(q, empty)(const q()* q);
 
-#endif
+void        qfunc(q, print)(q()* q);
