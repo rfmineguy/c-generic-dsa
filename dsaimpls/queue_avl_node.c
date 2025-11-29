683c498347ce3694b2b1507f48dbaf4546f951ba
Riley Fischer <rfmineguy@gmail.com>
2025-11-29 00:49:58 -0800

    dsaimpl: fix queue_avl_node.c

--- dsaimpls/queue_avl_node.c
+++ dsaimpls/queue_avl_node.c
@@ -1,6 +1,6 @@
 #include "../dsalib/avl.h"
-#define type_type avl_node
-#define type_name avl_node
-#define ALLOW_DIRECT_INCLUDE
+#define q_type_type avl_node
+#define q_type_name avl_node
+#define Q_ALLOW_DIRECT_INCLUDE
 
 #include "../dsalib/queue.c"
