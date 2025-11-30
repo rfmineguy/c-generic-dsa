bdf68487bffa4eb8926a9e70b7918d02866996fd
Riley Fischer <rfmineguy@gmail.com>
2025-11-30 08:56:25 -0800

    dsaimpls(stack_int): create integer stack specialization

---
+++ dsaimpls/stack_int.c
@@ -0,0 +1,8 @@
+#define stack_type_type int
+#define stack_type_name int
+#include <stdio.h>
+
+void stack_int_print_val(int v) {
+  printf("%d", v);
+}
+#include "../dsalib/stack.c"
---
+++ dsaimpls/stack_int.h
@@ -0,0 +1,3 @@
+#define stack_type_type int
+#define stack_type_name int
+#include "../dsalib/stack.h"
