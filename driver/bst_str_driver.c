970a934ddb2aaafdf1721f6945b7d582b1c25d7b
Riley Fischer <rfmineguy@gmail.com>
2025-11-29 22:06:06 -0800

    driver(bst_str): use more human readable strings instead of numbers

--- driver/bst_str_driver.c
+++ driver/bst_str_driver.c
@@ -53,34 +53,35 @@ void bst_str_driver_iterator() {
 
 void bst_str_driver_no_iterator() {
   bst_str bst = bst_str_new();
-  bst_str_insert(&bst, "5");
-  bst_str_insert(&bst, "20");
-  bst_str_insert(&bst, "5");
-  bst_str_insert(&bst, "2");
-  bst_str_insert(&bst, "8");
-  bst_str_insert(&bst, "14");
-  bst_str_insert(&bst, "12");
-  bst_str_insert(&bst, "11");
-  bst_str_insert(&bst, "16");
+  bst_str_insert(&bst, "looker");
+  bst_str_insert(&bst, "pawn");
+  bst_str_insert(&bst, "looker");
+  bst_str_insert(&bst, "general");
+  bst_str_insert(&bst, "moniker");
+  bst_str_insert(&bst, "horror");
+  bst_str_insert(&bst, "general");
+  bst_str_insert(&bst, "cookie");
+  bst_str_insert(&bst, "brownie");
+  bst_str_print(&bst);
+  bst_str_print_dot(&bst);
+
+  bst_str_delete(&bst, "brownie");
+  bst_str_delete(&bst, "horror");
+  bst_str_delete(&bst, "general");
   bst_str_print(&bst);
 
-  bst_str_delete(&bst, "16");
-  bst_str_delete(&bst, "14");
-  bst_str_delete(&bst, "12");
+  bst_str_delete(&bst, "cookie");
+  bst_str_delete(&bst, "cookie");
   bst_str_print(&bst);
 
-  bst_str_delete(&bst, "11");
-  bst_str_delete(&bst, "11");
+  bst_str_delete(&bst, "moniker");
   bst_str_print(&bst);
 
-  bst_str_delete(&bst, "8");
+  bst_str_delete(&bst, "looker");
+  bst_str_delete(&bst, "pawn");
   bst_str_print(&bst);
 
-  bst_str_delete(&bst, "5");
-  bst_str_delete(&bst, "20");
-  bst_str_print(&bst);
-
-  bst_str_delete(&bst, "2");
+  bst_str_delete(&bst, "general");
   bst_str_print(&bst);
   bst_str_free(&bst);
 
