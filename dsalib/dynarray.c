b028882640f6c62f008035f36953a3bf796e2163
Riley Fischer <rfmineguy@gmail.com>
2025-12-02 11:13:33 -0800

    dsalib(dynarray): implement grow function, for when the dynarray needs to resize (factor of 2)

--- dsalib/dynarray.c
+++ dsalib/dynarray.c
@@ -1,6 +1,13 @@
 #include "dynarray.h"
 #include <stdlib.h>
 
+bool dafunc(grow)(da()* da) {
+  da->buffer = realloc(da->buffer, da->capacity * 2 * sizeof(da_type_type));
+  if (!da->buffer) return false;
+  da->capacity *= 2;
+  return true;
+}
+
 da() dafunc(new)(int initial_capacity) {
   da() d = (da()) {.capacity = initial_capacity};
   d.buffer = calloc(initial_capacity, sizeof(da_type_type));
