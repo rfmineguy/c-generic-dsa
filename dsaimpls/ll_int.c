49ddbbe9418d93ea67d8128e202534375a9c2263
Riley Fischer <rfmineguy@gmail.com>
2025-12-01 19:04:08 -0800

    dsaimpls(ll_int): create integer specialization of linkedlist

---
+++ dsaimpls/ll_int.c
@@ -0,0 +1,9 @@
+#define ll_type_name int
+#define ll_type_type int
+#include <stdio.h>
+
+void ll_int_print_node_val(int v, FILE *f) {
+  fprintf(f, "%d", v);
+}
+
+#include "../dsalib/linkedlist.c"
---
+++ dsaimpls/ll_int.h
@@ -0,0 +1,3 @@
+#define ll_type_name int
+#define ll_type_type int
+#include "../dsalib/linkedlist.h"
